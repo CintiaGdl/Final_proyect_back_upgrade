5a349402cc14e194a293cc0b2288640644586d09
CintiaGdl <cintiamgori@gmail.com>
2022-04-23 20:44:22 +0200

    fix error routes user

--- src/api/users/users.routes.js
+++ src/api/users/users.routes.js
@@ -14,8 +14,8 @@ UserRoutes.post("/login", login);
 UserRoutes.post("/logout", [isAuth], logout);
 UserRoutes.get("/:id", [isAuth], getUser);
 UserRoutes.get("/", [isAuth], getAllUsers);
-UserRoutes.patch("/favProducts/:id", [isAuth], addFavBeverage);
-UserRoutes.patch("/favProducts/:id", [isAuth], addFavDessert);
-UserRoutes.patch("/favProducts/:id", [isAuth], addFavPizza);
+UserRoutes.patch("/favBeverages/:id", [isAuth], addFavBeverage);
+UserRoutes.patch("/favDesserts/:id", [isAuth], addFavDessert);
+UserRoutes.patch("/favPizzas/:id", [isAuth], addFavPizza);
 
 module.exports = UserRoutes;
